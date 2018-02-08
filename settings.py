8b774fe558f6675b6d0afe0498359d1a5a63342a
Shaun Molloy <shaunmolloy@gmail.com>
2018-02-08 08:46:24 +0000

    Added settings.py. - Gets string values for most vars in .env. - EMAILS var is parsed as json, to get it as a list.

---
+++ settings.py
@@ -0,0 +1,21 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+import json
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+"""
+" Vars from .env below
+"""
+LICENCE = os.environ.get("LICENCE")
+APPLICATION_REFERENCE = os.environ.get("APPLICATION_REFERENCE")
+
+EMAILS = json.loads(os.environ.get("EMAILS"))
+EMAIL_SERVER_URL = os.environ.get("EMAIL_SERVER_URL")
+EMAIL_SERVER_USERNAME = os.environ.get("EMAIL_SERVER_USERNAME")
+EMAIL_SERVER_PASSWORD = os.environ.get("EMAIL_SERVER_PASSWORD")
+
+TEST_DATE = os.environ.get("TEST_DATE")
+

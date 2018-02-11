ad4d2a14343b9ddfff31b55ec14005997320cf62
Shaun Molloy <shaunmolloy@gmail.com>
2018-02-11 23:45:49 +0000

    Added lib/helpers.py. + Added get_time static method: moved from browser class. + Added message static method: prints time and message. + Added error static message: prints time and message (in red).

---
+++ lib/helpers.py
@@ -0,0 +1,29 @@
+"""
+Helpers
+"""
+from datetime import datetime
+
+class helpers:
+    @staticmethod
+    def get_time():
+        # eg. Sun 11 Feb 20:00:00
+        # see: http://www.geezer.org/sw/mvform/doc/strftime.txt
+        return datetime.now().strftime("%a %d %b %X")
+
+    @staticmethod
+    def message(message):
+        # eg. [Sun 11 Feb 20:00:00] message
+        # see: https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+        print(
+            "%s[%s]%s" % ("\033[38;5;028m", helpers.get_time(), "\033[0m"),
+            message
+        )
+
+    @staticmethod
+    def error(message):
+        # eg. [Sun 11 Feb 20:00:00] exception
+        # see: self.message
+        print(
+            "%s[%s]%s" % ("\033[38;5;028m", helpers.get_time(), "\033[0m"),
+            "%s[%s]%s" % ("\033[38;5;200m", message, "\033[0m")
+        )

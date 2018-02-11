ace023d33ebcc7fb5e07f9f303b43a8a83d01347
Shaun Molloy <shaunmolloy@gmail.com>
2018-02-11 15:06:59 +0000

    Added lib/captcha.py. - Captcha class to handle download and display of captcha in terminal window. + Added download_image method. + Added show_image method: uses imgcat shell script to display in iTerm window. + Added delete_image method.

---
+++ lib/captcha.py
@@ -0,0 +1,30 @@
+"""
+Captcha
+"""
+from subprocess import call
+import os
+import urllib.request
+
+class captcha:
+    def __init__(self):
+        self.filename = "cache/captcha.jpeg"
+
+    """
+    Download image
+    """
+    def download_image(self, url):
+        urllib.request.urlretrieve(url, self.filename)
+
+    """
+    Show image in terminal
+    """
+    def show_image(self):
+        # TODO fallback to PIL.Image open/show
+        call(["./lib/imgcat", self.filename])
+        print("\n\n\n\n")
+
+    """
+    Delete image
+    """
+    def delete_image(self):
+        os.remove(self.filename)
